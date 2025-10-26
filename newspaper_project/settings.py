127752f22131dce750b08caa163aabec10354dea
peterkahumu <muhumukip@gmail.com>
2025-10-27 00:33:45 +0300

    feat: implement password reset functionality with corresponding templates and email backend configuration

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -143,3 +143,5 @@ LOGOUT_REDIRECT_URL = "login"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend"
