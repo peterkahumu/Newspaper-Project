d01b76408a28ef8ec700f0920aaa54da6e37671b
peterkahumu <muhumukip@gmail.com>
2025-10-26 21:42:49 +0300

    feat: add pages app with homepage view, navigation bar, and Bootstrap integration

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #3rd party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # local
     "accounts",
+    "pages",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -135,3 +140,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
---
+++ pages/tests.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class HomePageTests(SimpleTestCase):
+    def test_urls(self):
+        response = self.client.get("/")
+        response_name = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_name.status_code, 200)
+    
+    def test_homepageview(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "Home")
