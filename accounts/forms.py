27ba96590e90be9dd3d5b0d0ed9d257197394b9f
peterkahumu <muhumukip@gmail.com>
2025-10-26 15:23:11 +0300

    feat: add docstrings to custom user forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,11 +2,13 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    """Create a Custom user registration form to capture additional fields."""
     class Meta:
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('date_of_birth', )
 
 class CustomUserChangeForm(UserChangeForm):
+    """Create a custom form to change the user."""
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
